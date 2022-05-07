5190c557999e092c2cca2dc33a259a7b8515695f
Pierre Julien <pierrejulienpro@gmail.com>
2022-05-07 18:37:05 +0200

    Ajout des instructions permettant de désactiver les notifications des chaines suivies

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ async function main() {
   const user = await apiClient.users.getUserByName("href404");
   const follows = await user.getFollows();
   console.log("Follows counter :", follows.total);
+
+  // Waiting room for possibility to MR and/or improvments from Twitch ✨
+  // follows.data.forEach((follow) => follow.setLiveNotification(false));
 }
 
 main();
